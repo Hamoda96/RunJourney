be095ba17d8c18b6cbbaaebbbd55610b315fccac
Mahmoud Hamoda <31393608+Hamoda96@users.noreply.github.com>
2024-07-22 04:27:30 +0300

    add android application convention plugin (#2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,11 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.runjourney.android.application)
 }
 
 android {
     namespace = "com.hamoda.runjourney"
-    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.hamoda.runjourney"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -29,13 +22,7 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+
     buildFeatures {
         compose = true
     }
--- build-logic/convention/src/main/java/com/hamoda/convention/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.hamoda.convention
-
-class MyClass {
-}
