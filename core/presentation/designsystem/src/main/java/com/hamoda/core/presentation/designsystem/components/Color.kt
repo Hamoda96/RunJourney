40d76bd5ecc0752713bd6510ca82f9f952e5aacd
hamoda96 <mahmoud.m.hamoda@gmail.com>
2024-07-31 19:02:31 +0300

    add them for app

---
+++ core/presentation/designsystem/src/main/java/com/hamoda/core/presentation/designsystem/components/Color.kt
@@ -0,0 +1,15 @@
+package com.hamoda.core.presentation.designsystem.components
+
+import androidx.compose.ui.graphics.Color
+
+val RunJourneyGreen = Color(0xFF00F15E)
+val RunJourneyGreen30 = Color(0x4D00F15E)
+val RunJourneyGreen10 = Color(0x1A00F15E)
+val RunJourneyGreen5 = Color(0x0D00F15E)
+val RunJourneyBlack = Color(0xFF080707)
+val RunJourneyGray = Color(0xFF87938C)
+val RunJourneyGray40 = Color(0x6687938C)
+val RunJourneyDarkGray = Color(0xFF232624)
+val RunJourneyWhite = Color(0xFFFAFAFA)
+val RunJourneyDarkRed = Color(0xFFBB3D3D)
+val RunJourneyDarkRed5 = Color(0x0DBB3D3D)
