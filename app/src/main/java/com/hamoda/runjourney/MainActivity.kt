d05021919c62799e52f5ab362a3549c847bf3d39
hamoda96 <mahmoud.m.hamoda@gmail.com>
2024-10-01 05:14:17 +0300

    add splash screen and app icon

--- app/src/main/java/com/hamoda/runjourney/MainActivity.kt
+++ app/src/main/java/com/hamoda/runjourney/MainActivity.kt
@@ -5,26 +5,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.hamoda.core.presentation.designsystem.RunJourneyTheme
 import com.hamoda.runjourney.navigation.NavigationRoot
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.state.isCheckingAuth
+            }
+        }
+
         setContent {
             RunJourneyTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    val navController = rememberNavController()
-                    NavigationRoot(navController = navController)
+                    if (!viewModel.state.isCheckingAuth) {
+                        val navController = rememberNavController()
+                        NavigationRoot(
+                            navController = navController,
+                            isLoggedIn = viewModel.state.isLoggedIn
+                        )
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/hamoda/runjourney/MainState.kt
@@ -0,0 +1,6 @@
+package com.hamoda.runjourney
+
+data class MainState(
+    val isLoggedIn: Boolean = false,
+    val isCheckingAuth: Boolean = false
+)
