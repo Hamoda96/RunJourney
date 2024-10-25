a35119fdc55004f952e6a5475e0e8def3015cf6f
hamoda96 <mahmoud.m.hamoda@gmail.com>
2024-10-25 21:19:52 +0300

    add dialog of finish and resume running

--- run/presentation/src/main/java/com/hamoda/run/presntation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/hamoda/run/presntation/active_run/ActiveRunScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hamoda.core.presentation.designsystem.RunJourneyTheme
+import com.hamoda.core.presentation.designsystem.components.RunJourneyActionButton
 import com.hamoda.core.presentation.designsystem.components.RunJourneyDialog
 import com.hamoda.core.presentation.designsystem.components.RunJourneyFloatingActionButton
 import com.hamoda.core.presentation.designsystem.components.RunJourneyOutlinedActionButton
@@ -161,6 +162,36 @@ private fun ActiveRunScreen(
                     .fillMaxWidth()
             )
         }
+    }
+
+    if (!state.shouldTrack && state.hasStartedRunning) {
+        RunJourneyDialog(
+            title = stringResource(id = R.string.running_is_paused),
+            onDismiss = {
+                onAction(ActiveRunAction.OnResumeRunClick)
+            },
+            description = stringResource(id = R.string.resume_or_finish_run),
+            primaryButton = {
+                RunJourneyActionButton(
+                    text = stringResource(id = R.string.resume),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.OnResumeRunClick)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            },
+            secondaryButton = {
+                RunJourneyOutlinedActionButton(
+                    text = stringResource(id = R.string.finish),
+                    isLoading = state.isSavingRun,
+                    onClick = {
+                        onAction(ActiveRunAction.OnFinishRunClick)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        )
     }
 
     if (state.showLocationRationale || state.showNotificationRationale) {
