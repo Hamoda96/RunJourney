db36948092f6c424bafb413dbafc0842bd7d4767
hamoda96 <mahmoud.m.hamoda@gmail.com>
2024-11-09 14:25:05 +0200

    add runs data model and ui model with the mapper

---
+++ run/presentation/src/main/java/com/hamoda/run/presntation/active_run/mapper/toRunUi.kt
@@ -0,0 +1,33 @@
+package com.hamoda.run.presntation.active_run.mapper
+
+import com.hamoda.run.domain.Run
+import com.hamoda.core.domain.util.formatted
+import com.hamoda.core.domain.util.toFormattedKm
+import com.hamoda.core.domain.util.toFormattedKmh
+import com.hamoda.core.domain.util.toFormattedMeters
+import com.hamoda.core.domain.util.toFormattedPace
+import com.hamoda.run.presntation.active_run.model.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc
+        .withZoneSameInstant(ZoneId.systemDefault())
+
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+    val distanceKm = distanceMeters / 1000.0
+
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
